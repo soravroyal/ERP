9d19b4b7addafd2acc68dc4e78905b59ccb219d5
spanghel <spanghel@4f4f039f-793a-4267-b908-41e57281febd>
2011-02-07 08:53:25 +0000

    Reorganise deltascripts

--- Website/SQLScripts/DeltaScripts/cms/UpdateAreaSearchIntroText.sql
+++
@@ -1,5 +0,0 @@
-
-
-update dbo.tAT_ResourceValue
-set ResourceValue = 'This report will display the aggregated releases and transfers in a specific area'
-where ResourceValue = 'This report will display the aggregated releases and transfers of a specific area'
