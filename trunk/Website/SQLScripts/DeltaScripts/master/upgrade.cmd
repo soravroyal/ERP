7e9f78720046b4900015f5c26d7a99183f0c51b0
spanghel <spanghel@4f4f039f-793a-4267-b908-41e57281febd>
2011-02-15 10:26:51 +0000

    #3 Added version number to tables

---
+++ trunk/Website/SQLScripts/DeltaScripts/master/upgrade.cmd
@@ -0,0 +1,13 @@
+echo update
+
+set SQLCMDSERVER=sdkcga6302
+set SQLCMDDBNAME=EPRTRmaster
+set SQLCMDUSER=sa
+set SQLCMDPASSWORD=tmggis
+
+
+rem update from ver 2.0
+SET basedir=2.0
+sqlcmd -i %basedir%\00_upgrade.sql
+
+PAUSE
