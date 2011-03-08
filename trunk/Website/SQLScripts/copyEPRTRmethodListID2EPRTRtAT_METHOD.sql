cb3f9b2a1f082721089b4254b230bfab30d50e45
starzwol <starzwol@4f4f039f-793a-4267-b908-41e57281febd>
2011-03-08 12:45:20 +0000

    procedure pAT_METHOD extracted

--- trunk/Website/SQLScripts/copyEPRTRmethodListID2EPRTRtAT_METHOD.sql
+++ trunk/Website/SQLScripts/copyEPRTRmethodListID2EPRTRtAT_METHOD.sql
@@ -4,38 +4,48 @@
 --	copyEPRTRmasterViews2EPRTRwebTables is called
 -- 	(when copying newly imported data from a member state to the WEB DB)
 --
---	WOS, Atkins 0202201
+--	WOS, Atkins 02032010
 --
 --------------------------------------------------------------------------------
 
-use EPRTRmaster
+exec EPRTRmaster.dbo.pAT_METHOD
 
-truncate table EPRTRmaster.dbo.tAT_METHOD
- 
-insert into EPRTRmaster.dbo.tAT_METHOD (MethodListID,MethodCode,MethodDesignation)
-select
-	mu.MethodListID,
-	lov.Code,
-	mu.MethodDesignation
-from METHODUSED mu
-inner join LOV_METHODTYPE lov
-on mu.LOV_MethodTypeID = lov.LOV_MethodTypeID
-where mu.MethodListID in (
-	select mu.MethodListID from METHODUSED mu
-	group by mu.MethodListID
-	having COUNT(mu.MethodListID) = 1
-	)
-go 
+--/****** Object:  StoredProcedure [dbo].[pAT_WASTETRANSFER]    Script Date: 03/08/2011 13:06:42 ******/
+--SET ANSI_NULLS ON
+--GO
 
-insert into EPRTRmaster.dbo.tAT_METHOD (MethodListID,MethodCode,MethodDesignation)
-select
-	MethodListID, 
-	EPRTRmaster.dbo.fAT_GETMETHODTYPE(MethodListID),
-	EPRTRmaster.dbo.fAT_GETMETHODDESIGNATION(MethodListID)
-from(
-		select mu.MethodListID from METHODUSED mu
-		group by mu.MethodListID
-		having COUNT(mu.MethodListID) > 1
-)i
+--SET QUOTED_IDENTIFIER ON
+--GO
 
---select * from EPRTRmaster.dbo.tAT_METHOD where MethodCode like '%|%' order by 4
+--create procedure [dbo].[pAT_METHOD]
+--as 
+--begin
+
+--	truncate table EPRTRmaster.dbo.tAT_METHOD
+	 
+--	insert into EPRTRmaster.dbo.tAT_METHOD (MethodListID,MethodCode,MethodDesignation)
+--	select
+--		mu.MethodListID,
+--		lov.Code,
+--		mu.MethodDesignation
+--	from METHODUSED mu
+--	inner join LOV_METHODTYPE lov
+--	on mu.LOV_MethodTypeID = lov.LOV_MethodTypeID
+--	where mu.MethodListID in (
+--		select mu.MethodListID from METHODUSED mu
+--		group by mu.MethodListID
+--		having COUNT(mu.MethodListID) = 1
+--		)
+
+--	insert into EPRTRmaster.dbo.tAT_METHOD (MethodListID,MethodCode,MethodDesignation)
+--	select
+--		MethodListID, 
+--		EPRTRmaster.dbo.fAT_GETMETHODTYPE(MethodListID),
+--		EPRTRmaster.dbo.fAT_GETMETHODDESIGNATION(MethodListID)
+--	from(
+--			select mu.MethodListID from METHODUSED mu
+--			group by mu.MethodListID
+--			having COUNT(mu.MethodListID) > 1
+--	)i
+
+--end
