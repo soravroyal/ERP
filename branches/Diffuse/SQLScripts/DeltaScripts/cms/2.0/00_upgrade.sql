150c306310259dd6628278f2b0ac664fed2eca78
spanghel <spanghel@4f4f039f-793a-4267-b908-41e57281febd>
2011-05-03 09:12:04 +0000

    Added version to DB

---
+++ branches/Diffuse/SQLScripts/DeltaScripts/cms/2.0/00_upgrade.sql
@@ -0,0 +1,22 @@
+/*
+Upgrading CMS database from ver 1.9 (prior to version in DB)
+*/
+
+--set new version number
+:Setvar major 2
+:Setvar minor 0
+
+PRINT 'UPDATING: $(SQLCMDSERVER)\$(SQLCMDDBNAME) FROM VERSION $(basedir) . . .'
+
+:On Error exit
+:out upgrade.log
+
+-- Include deltascripts here
+:r$(basedir)\..\..\common\AddVersion.sql
+go
+
+--Uncomment this when released (will only insert version no. once) :
+/*
+IF NOT EXISTS (SELECT * FROM tAT_version WHERE Major_version = $(major) AND Minor_version = $(minor))
+	INSERT INTO tAT_version(Major_version, Minor_version) VALUES ($(major), $(minor));
+*/
