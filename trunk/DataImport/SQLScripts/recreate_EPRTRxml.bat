c6a9facbf7b692f4d470a4d818ac857897d43d9a
starzwol <starzwol@4f4f039f-793a-4267-b908-41e57281febd>
2012-03-28 11:52:02 +0000

    first import

---
+++ trunk/DataImport/SQLScripts/recreate_EPRTRxml.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+REM See http://msdn.microsoft.com/en-us/library/ms162773.aspx for reference.
+REM Set environment variables SQLCMDUSER, SQLCMDPASSWORD and SQLCMDSERVER
+REM before running this script.
+
+SET basedir=%1
+
+REM Clear existing database:
+SET SQLCMDDBNAME=master
+sqlcmd -Q "ALTER DATABASE EPRTRxml SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE EPRTRxml;"
+sqlcmd -Q "CREATE DATABASE EPRTRxml COLLATE Latin1_General_CI_AS;"
+
+REM Create database structure:
+SET SQLCMDDBNAME=EPRTRxml
+sqlcmd -i %basedir%\CreateTables_EPRTRxml.sql
+sqlcmd -i %basedir%\CreateReferences_EPRTRxml.sql
+sqlcmd -i %basedir%\Add_aux_cols_4_xmlimport.sql
+sqlcmd -i %basedir%\SP_FindPreviousReferences.sql
+sqlcmd -i %basedir%\copyXMLdata2EPRTR.sql
+sqlcmd -i %basedir%\EPRTRXML_validate_procedure.sql
