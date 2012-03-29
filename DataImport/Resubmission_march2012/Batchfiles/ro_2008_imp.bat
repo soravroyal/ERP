9d89305e2b6c10837aac3656a7e69d5950095238
starzwol <starzwol@4f4f039f-793a-4267-b908-41e57281febd>
2012-03-29 11:12:11 +0000

    All files data and batch for submission march 2012

---
+++ DataImport/Resubmission_march2012/Batchfiles/ro_2008_imp.bat
@@ -0,0 +1,26 @@
+echo off
+echo Importing Romania 2008
+echo -----------------------------------------------
+
+SET sqlscript_dir=%1
+SET mapforce_dir=%2
+SET data_dir=%3
+SET doValidate=%4
+
+rem Recreating EPRTRxml database
+call %sqlscript_dir%\recreate_EPRTRxml.bat %1 %sqlscript_dir%
+rem Importing xml file into EPRTRxml database 
+call %mapforce_dir%\EPRTR_Import_CMD.exe "%data_dir%\RO_EPRTR-2008.xml" %SQLCMDSERVER% %SQLCMDUSER% %SQLCMDPASSWORD%
+rem -----------------------------------------------
+
+if "%doValidate%"=="true" (
+  SET SQLCMDDBNAME=EPRTRxml
+  echo Validating data
+  sqlcmd -Q "EXEC EPRTRxml.dbo.validate_xml_data"
+  echo -----------------------------------------------
+)
+
+echo Copying data from xml to master
+sqlcmd -i %sqlscript_dir%\reset_cols_4_xmlimport.sql
+sqlcmd -Q "EXEC EPRTRxml.dbo.import_xml @pCDRURL=N'http://cdr.eionet.europa.eu/ro/eu/eprtrdat/envt2c2ow',@pCDRUploaded=N'2012-03-19 14:36:00', @pCDRReleased=N'2012-03-19 14:39:21', @pResubmitReason='E-PRTR data 2008';"
+echo -----------------------------------------------
