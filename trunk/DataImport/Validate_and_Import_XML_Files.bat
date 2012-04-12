9c8a1a459633f741156a7b39cb843ac6c2309cee
spanghel <spanghel@4f4f039f-793a-4267-b908-41e57281febd>
2012-04-12 13:47:32 +0000

    chnaged import script to read info form config file. One batcfile per dataset no longer needed

--- trunk/DataImport/Validate_and_Import_XML_Files.bat
+++ trunk/DataImport/Validate_and_Import_XML_Files.bat
@@ -1,23 +1,36 @@
 echo off
-rem set /p SQLCMDSERVER= Type in name of server and press Enter:
-rem set /p SQLCMDUSER= Type in name of database user and press Enter:
-rem set /p SQLCMDPASSWORD= Type in password for database user and press Enter:
+rem set up database server, user and password
+SET SQLCMDSERVER=sdkcga6332
+SET SQLCMDUSER=gis
+SET SQLCMDPASSWORD=tmggis
 
-set SQLCMDSERVER=sdkcga6332
-set SQLCMDUSER=gis
-set SQLCMDPASSWORD=tmggis
+rem set up base directory of scripts and data
+SET basedir=D:\EPRTR\DataImport
+SET data_dir=D:\EPRTR\XML_DOWNLOAD\submission_2012_04_15
 
+rem set doValidate true/false to indicate if validations are to be performed or not 
+SET doValidate=false
 
-for %%f in (*_imp.bat) do (
-	echo %%f
-	rem parameter %1: sqlscript_dir
-	rem parameter %2: mapforce_dir
-	rem parameter %3: data_dir (location of XML files)
-	rem parameter %4: doValidate (true/false)
-	call %%f > %%f.log 	%1 D:\EPRTRimport\EIONET\SQLScripts 	%2 D:\EPRTRimport\XML-import\MAPFORCE 	%3 D:\EPRTRimport\XML-import\XML_DOWNLOAD\submission_2012_03_01 %4 true
+SET sqlscript_dir=%basedir%\SQLScripts
+SET mapforce_dir=%basedir%\MAPFORCE
+SET config=%data_dir%\00_config.txt
+
+rem start importing all files as defined in config file
+if not exist %data_dir%\logs mkdir %data_dir%\logs
+
+SETLOCAL EnableDelayedExpansion
+
+for /f "eol=# tokens=1,2,3,4,5,6,7 delims=," %%a in (%config%) do (
+
+	SET title=%%c %%a
+	SET filename=%%b_EPRTR-%%a.xml
+	SET cdrUrl=%%d
+	SET cdrUploaded=%%e
+	SET cdrReleased=%%f
+	SET cdrDescription=%%g
+
+	echo Importing:!filename!
+	call Validate_and_Import_XML_File.bat > %data_dir%\logs\!filename!.log %basedir%  %data_dir%  %doValidate%  "!title!"  !filename!  "!cdrUrl!"  "!cdrUploaded!"  "!cdrReleased!"  "!cdrDescription!"
 )
-set /p name=press enter
 
-	
-	
-	
+set /p name=press enter
