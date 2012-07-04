a53c4e4bd50360dc444c203b601c6a9b6e6910e4
rutkakos <rutkakos@4f4f039f-793a-4267-b908-41e57281febd>
2012-07-04 14:29:30 +0000

    Added initial links between exported entities.

--- trunk/RDFExport/MakeProperties/Program.cs
+++ trunk/RDFExport/MakeProperties/Program.cs
@@ -16,44 +16,57 @@ namespace MakeProperties
             Password=tmggis;
             Trusted_Connection=False;";
 
-        static string[] ExportableTables = new string[]
+        static DatabaseTable[] ExportableTables = new DatabaseTable[]
         {
-            "LOV_AnnexIActivity",
-		    "LOV_AreaGroup", 
-		    "LOV_Confidentiality",
-		    "LOV_CoordinateSystem",
-		    "LOV_Country",
-		    "LOV_CountryAreaGroup",
-		    "LOV_Medium",
-		    "LOV_MethodBasis",
-		    "LOV_MethodType",
-		    "LOV_NACEActivity",
-		    "LOV_NUTSRegion",
-		    "LOV_Pollutant",
-		    "LOV_PollutantThreshold",
-		    "LOV_RiverBasinDistrict",
-		    "LOV_Status",
-		    "LOV_Unit",
-		    "LOV_WasteThreshold",
-		    "LOV_WasteTreatment",
-		    "LOV_WasteType",
-		    "PUBLISH_Activity",
-		    "PUBLISH_Eper2EPRTR_AnnexIActivity",
-		    "PUBLISH_Eper2EPRTR_NACEActivity",
-		    "PUBLISH_FacilityID_Changes",
-		    "PUBLISH_FacilityReport",
-		    "PUBLISH_PollutantRelease",
-		    "PUBLISH_PollutantReleaseAndTransferReport",
-		    "PUBLISH_PollutantReleaseMethodUsed",
-		    "PUBLISH_PollutantTransfer",
-		    "PUBLISH_PollutantTransferMethodUsed",
-		    "PUBLISH_UploadedReports",
-		    "PUBLISH_WasteTransfer",
-		    "PUBLISH_WasteTransferMethodUsed"
+            new DatabaseTable("LOV_AnnexIActivity", "Code", true),
+		    new DatabaseTable("LOV_AreaGroup", "Code", true), 
+		    new DatabaseTable("LOV_Confidentiality", "Code", true),
+		    new DatabaseTable("LOV_CoordinateSystem", "Code", true),
+		    new DatabaseTable("LOV_Country", "Code", true),
+		    // TODO: new DatabaseTable("LOV_CountryAreaGroup", ??),
+		    new DatabaseTable("LOV_Medium", "Code", true),
+		    new DatabaseTable("LOV_MethodBasis", "Code", true),
+		    new DatabaseTable("LOV_MethodType", "Code", true),
+		    new DatabaseTable("LOV_NACEActivity", "Code", true),
+		    new DatabaseTable("LOV_NUTSRegion", "Code", true),
+		    new DatabaseTable("LOV_Pollutant", "Code", true),
+		    // TODO: new DatabaseTable("LOV_PollutantThreshold", ??),
+		    new DatabaseTable("LOV_RiverBasinDistrict", "Code", true),
+		    new DatabaseTable("LOV_Status", "Code", true),
+		    new DatabaseTable("LOV_Unit", "Code", true),
+		    // TODO: new DatabaseTable("LOV_WasteThreshold", ??),
+		    new DatabaseTable("LOV_WasteTreatment", "Code", true),
+		    new DatabaseTable("LOV_WasteType", "Code", true),
+            new DatabaseTable("Facility", "FacilityID", true),
+		    new DatabaseTable("PUBLISH_Activity"),
+		    new DatabaseTable("PUBLISH_Eper2EPRTR_AnnexIActivity"),
+		    new DatabaseTable("PUBLISH_Eper2EPRTR_NACEActivity"),
+		    new DatabaseTable("PUBLISH_FacilityID_Changes"),
+		    new DatabaseTable("PUBLISH_FacilityReport"),
+		    new DatabaseTable("PUBLISH_PollutantRelease"),
+		    new DatabaseTable("PUBLISH_PollutantReleaseAndTransferReport"),
+		    new DatabaseTable("PUBLISH_PollutantReleaseMethodUsed"),
+		    new DatabaseTable("PUBLISH_PollutantTransfer"),
+		    new DatabaseTable("PUBLISH_PollutantTransferMethodUsed"),
+		    new DatabaseTable("PUBLISH_UploadedReports"),
+		    new DatabaseTable("PUBLISH_WasteTransfer"),
+		    new DatabaseTable("PUBLISH_WasteTransferMethodUsed")
         };
 
         static AliasColumn[] AliasColumns = new AliasColumn[] 
         {
+            new AliasColumn
+            {
+                TableName = "PUBLISH_Activity",
+                ColumnName = "FacilityReportID",
+                ForeignPropertyName = "facilityReport"
+            },
+            new AliasColumn
+            {
+                TableName = "PUBLISH_Activity",
+                ColumnName = "AnnexIActivityCode",
+                ForeignPropertyName = "annexIActivity"
+            },
             new AliasColumn
             {
                 TableName = "PUBLISH_FACILITYREPORT",
@@ -140,9 +153,27 @@ namespace MakeProperties
             },
             new AliasColumn
             {
+                TableName = "PUBLISH_POLLUTANTTRANSFER",
+                ColumnName = "MethodBasisCode",
+                ForeignPropertyName = "methodBasis"
+            },
+            new AliasColumn
+            {
+                TableName = "PUBLISH_POLLUTANTTRANSFER",
+                ColumnName = "QuantityUnitCode",
+                ForeignPropertyName = "unit"
+            },
+            new AliasColumn
+            {
                 TableName = "PUBLISH_WASTETRANSFER",
                 ColumnName = "FacilityReportID",
                 ForeignPropertyName = "facilityReport"
+            }, 
+            new AliasColumn
+            {
+                TableName = "PUBLISH_WASTETRANSFERMETHODUSED",
+                ColumnName = "MethodTypeCode",
+                ForeignPropertyName = "methodType"
             }, 
             new AliasColumn
             {
@@ -170,6 +201,30 @@ namespace MakeProperties
             },
             new AliasColumn
             {
+                TableName = "PUBLISH_WASTETRANSFER",
+                ColumnName = "WasteTypeCode",
+                ForeignPropertyName = "forWasteType"
+            },
+            new AliasColumn
+            {
+                TableName = "PUBLISH_WASTETRANSFER",
+                ColumnName = "WasteTreatment",
+                ForeignPropertyName = "forWasteTreatment"
+            },
+            new AliasColumn
+            {
+                TableName = "PUBLISH_WASTETRANSFER",
+                ColumnName = "MethodBasisCode",
+                ForeignPropertyName = "forMethod"
+            },
+            new AliasColumn
+            {
+                TableName = "PUBLISH_WASTETRANSFER",
+                ColumnName = "QuantityUnitCode",
+                ForeignPropertyName = "unit"
+            },
+            new AliasColumn
+            {
                 TableName = "LOV_WASTETYPE",
                 ColumnName = "ParentID",
                 ForeignPropertyName = "parentType"
@@ -190,6 +245,7 @@ namespace MakeProperties
 
         static void Main(string[] args)
         {
+            // Generate rdfexport.properties
             using (SqlConnection conn = new SqlConnection(CONN_STRING))
             {
                 conn.Open();
@@ -202,15 +258,18 @@ namespace MakeProperties
 
                 conn.Close();
             }
+
+            // Generate Makefile
+            WriteMakeFile(ExportableTables);
         }
 
-        static string CreateTableListProperty(string[] tables)
+        static string CreateTableListProperty(DatabaseTable[] tables)
         {
             StringBuilder sb = new StringBuilder();
             sb.Append("tables = ");
             for (int i = 0; i < tables.Length; i++)
             {
-                sb.Append(TrimTablePrefix(tables[i]));
+                sb.Append(TrimTablePrefix(tables[i].TableName));
                 if (i < tables.Length - 1)
                 {
                     sb.Append(" \\\n\t\t");
@@ -219,25 +278,25 @@ namespace MakeProperties
             return sb.ToString();
         }
 
-        static string CreateQueryListProperties(SqlConnection conn, string[] tables)
+        static string CreateQueryListProperties(SqlConnection conn, DatabaseTable[] tables)
         {
             StringBuilder sb = new StringBuilder();
-            foreach (string table in tables)
+            foreach (DatabaseTable table in tables)
             {
-                string className = ToClassName(table);
-                sb.Append(string.Format("{0}.class = {1}\n", className, TrimTablePrefix(table)));
+                string className = ToClassName(table.TableName);
+                sb.Append(string.Format("{0}.class = {1}\n", className, TrimTablePrefix(table.TableName)));
                 sb.Append(string.Format("{0}.query = {1}\n", className, CreateTableQuery(conn, table)));
                 sb.Append("\n");
             }
             return sb.ToString();
         }
 
-        static string CreateTableQuery(SqlConnection conn, string table)
+        static string CreateTableQuery(SqlConnection conn, DatabaseTable table)
         {
             SqlCommand cmd = new SqlCommand(string.Format(@"
                                                 SELECT NAME FROM sys.columns 
                                                 WHERE object_id = OBJECT_ID('{0}')",
-                                            table), conn);
+                                            table.TableName), conn);
             StringBuilder sb = new StringBuilder();
             sb.Append("SELECT ");
 
@@ -259,6 +318,25 @@ namespace MakeProperties
             }
             reader.Close();
 
+            if (table.IdentifierColumnName != null)
+            {
+                foreach (string col in columnNames.ToList())
+                {
+                    if (col.Equals(table.IdentifierColumnName, 
+                        StringComparison.InvariantCultureIgnoreCase) 
+                        /*
+                         ||
+                        col.StartsWith("LOV_", StringComparison.InvariantCultureIgnoreCase) &&
+                        col.EndsWith("ID", StringComparison.InvariantCultureIgnoreCase)
+                         */)
+                    {
+                        columnNames.RemoveAt(columnNames.IndexOf(col));
+                    }
+                }
+
+                columnNames.Insert(0, table.IdentifierColumnName);
+            }
+
             string separator = "";
             foreach (string columnName in columnNames)
             {
@@ -268,13 +346,13 @@ namespace MakeProperties
                 sb.Append(columnName);
 
                 if (AliasColumns.Count(c =>
-                    c.TableName.Equals(table, StringComparison.InvariantCultureIgnoreCase)
+                    c.TableName.Equals(table.TableName, StringComparison.InvariantCultureIgnoreCase)
                     &&
                     c.ColumnName.Equals(columnName, StringComparison.InvariantCultureIgnoreCase)
                     ) == 1)
                 {
                     AliasColumn foreignColumn = AliasColumns.Single(c =>
-                    c.TableName.Equals(table, StringComparison.InvariantCultureIgnoreCase)
+                    c.TableName.Equals(table.TableName, StringComparison.InvariantCultureIgnoreCase)
                     &&
                     c.ColumnName.Equals(columnName, StringComparison.InvariantCultureIgnoreCase)
                     );
@@ -284,7 +362,7 @@ namespace MakeProperties
                 }
             } 
 
-            if (table.ToUpper().StartsWith("LOV_") && 
+            if (table.TableName.ToUpper().StartsWith("LOV_") && 
                 columnNames.Contains("Code") &&
                 columnNames.Contains("Name"))
             {
@@ -292,7 +370,7 @@ namespace MakeProperties
                 sb.Append("Code + ' - ' + Name AS 'rdfs:label'");
             }
 
-            sb.Append(" \\\n FROM " + table);
+            sb.Append(" \\\n FROM " + table.TableName);
             return sb.ToString();
         }
 
@@ -325,6 +403,47 @@ namespace MakeProperties
             }
 
             return char.ToLower(className[0]) + className.Substring(1);
+        }
+
+        static void WriteMakeFile(DatabaseTable[] databaseTables)
+        {
+            TextReader tr = new StreamReader("Makefile.template");
+            string templateFileContents = tr.ReadToEnd();
+
+            StringBuilder lookupTables = new StringBuilder();
+            string lookupTablesSeparator = "";
+
+            StringBuilder rdfExportTables = new StringBuilder();
+            string rdfExportTablesSeparator = "";
+
+            foreach (DatabaseTable databaseTable in databaseTables)
+            {
+                if (databaseTable.IsLookupTable)
+                {
+                    lookupTables.Append(lookupTablesSeparator);
+                    lookupTablesSeparator = " ";
+                    lookupTables.Append(ToClassName(databaseTable.TableName));
+                }
+                else
+                {
+                    rdfExportTables.Append(rdfExportTablesSeparator);
+                    rdfExportTablesSeparator = "\n\t";
+                    rdfExportTables.Append(String.Format("java -cp $(CLASSPATH) GenerateRDF {0} > {1}.rdf",
+                        ToClassName(databaseTable.TableName),
+                        TrimTablePrefix(databaseTable.TableName)));
+                }
+            }
+
+            templateFileContents = templateFileContents.Replace(
+                "{LookupTables}", lookupTables.ToString());
+            templateFileContents = templateFileContents.Replace(
+                "{rdfExportTables}", rdfExportTables.ToString());
+
+            using (StreamWriter propertiesFile = new StreamWriter(@"Makefile"))
+            {
+                propertiesFile.WriteLine(templateFileContents);
+                propertiesFile.Flush();
+            }
         }
     }
 }
