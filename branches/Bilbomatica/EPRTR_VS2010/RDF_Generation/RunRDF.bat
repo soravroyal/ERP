67af582c9d7ddee4faccf908e27b98601aa23fc2
roug <roug@4f4f039f-793a-4267-b908-41e57281febd>
2013-05-16 11:42:41 +0000

    An inclusion of RDFExport that uses svn:externals

---
+++ branches/Bilbomatica/EPRTR_VS2010/RDF_Generation/RunRDF.bat
@@ -0,0 +1,28 @@
+@echo off
+echo Building the library - uncomment
+rem cd RDFExport
+rem mvn -Dmaven.test.skip=true install
+rem cd ..
+
+echo Starting RDF files generation
+
+rmdir /s /q RDFfiles
+
+mkdir RDFfiles
+
+echo generating lookuptables.rdf...
+REM java -cp  ./RDFExport/target/rdf-exporter-1.0-SNAPSHOT.jar;./jtds-1.2.5.jar eionet.rdfexport.Execute AnnexIActivity AreaGroup Confidentiality Country medium pollutant pollutantthreshold wastethreshold wastetype unit method methodtype rbd -f rdfexport.properties -o ./RDFfiles/lookuptables.rdf
+java -cp  ./RDFExport/target/rdf-exporter-1.0-SNAPSHOT.jar;./jtds-1.2.5.jar eionet.rdfexport.Execute annexIActivity areaGroup confidentiality coordinateSystem country medium methodBasis methodType nACEActivity nUTSRegion pollutant riverBasinDistrict status unit wasteTreatment wasteType pollutantthreshold wastethreshold unit method methodtype rbd  -f rdfexport.properties -o ./RDFfiles/lookuptables.rdf 
+
+echo generating facility.rdf...
+java -cp  ./RDFExport/target/rdf-exporter-1.0-SNAPSHOT.jar;./jtds-1.2.5.jar eionet.rdfexport.Execute facility -f rdfexport.properties -o ./RDFfiles/Facility.rdf
+
+echo Compressing files... Why?
+
+tar -cf RDFfiles.gz RDFfiles/
+
+rmdir /s /q RDFfiles
+
+echo Process finished.
+	
+pause
